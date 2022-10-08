943b3ebb6ec2f5b34db0a273389b7a43cb9bec32
charburgx <charburgx@gmail.com>
2022-10-07 19:38:44 -0500

    feat(api): export tree info types

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,4 @@
 export { getSymbolType, multilineTypeToString } from "./util"
 export { recursivelyExpandType } from "./merge"
 export { generateTypeTree } from "./tree"
+export { TypeInfo, SymbolInfo, TypeInfoNode, SignatureInfo, TypeId, IndexInfo, TypeInfoKind, TypeParameterInfo } from "./types"
