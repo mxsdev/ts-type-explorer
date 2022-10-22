64ad805ce1b93942d957c3e4ba919f9e9e0f026d
charburgx <charburgx@gmail.com>
2022-10-21 23:59:00 -0500

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    ignorePatterns: ["tests/cases/**", "**/out/**", "**/dist/**", "**/*.js", "**/*.cjs"],
+    root: true,
+};
